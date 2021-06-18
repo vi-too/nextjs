e5200c726bcf9b386d54f5991b7c66d2787b7427
Vi Too <virnont@gmail.com>
2021-06-18 20:49:56 +0800

    Add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
+import Head from 'next/head';
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="px-5 text-2xl">
-        Hello! Thank you for visiting this page, unfortunately, this page is
-        still under heavy development. Please visit this page again at a later
-        time.
-      </h1>
-    </div>
+    <>
+      <Head>
+        <title>Virnon Tutor - Portfolio</title>
+      </Head>
+      <div className="flex items-center justify-center min-h-screen">
+        <h1 className="px-5 text-2xl">
+          Hello! Thank you for visiting this page, unfortunately, this page is
+          still under heavy development. Please visit this page again at a later
+          time.
+        </h1>
+      </div>
+    </>
   );
 }
